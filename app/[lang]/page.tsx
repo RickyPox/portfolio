e95adc3b56d0b9c162bcfb46ddda7fb291189fbb
Ricky <ricardojsribeiro99@gmail.com>
2025-10-22 16:12:58 +0100

    TechStacks

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import GetToKnow from "@/components/GetToKnow";
 import Hero from "@/components/Hero";
+import TechStacks from "@/components/TechStacks";
 import { useLanguage } from "@/context/LanguageContext";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
         <div>
             <Hero></Hero>
             <GetToKnow></GetToKnow>
+            <TechStacks></TechStacks>
         </div>
     );
 }
--- components/TechStacks.tsx
+++ components/TechStacks.tsx
@@ -1,3 +1,5 @@
+import { useLanguage } from "@/context/LanguageContext";
+
 export default function TechStacks() {
     const techs = [
         { name: "Html 5", img: "" },
@@ -14,5 +16,25 @@ export default function TechStacks() {
         { name: "Github", img: "" },
     ];
 
-    return <div></div>;
+    const dict = useLanguage();
+    return (
+        <div className="main-grid">
+            <div className="col-start-2 col-span-6 grid grid-cols-6 gap-[20px]">
+                <div className="col-start-1 col-span-full flex gap-x-[20px]">
+                    <h3>/////////////////////////////////////</h3>
+                    <h3>{dict.tech.title}</h3>
+                </div>
+                {techs.map((tech, i) => (
+                    <div key={i} className=" col-span-1 tech-card">
+                        <img alt={tech.name} src={tech.img} />
+                        <h4>{tech.name}</h4>
+                    </div>
+                ))}
+                <div className="col-start-1 col-span-full flex gap-x-[20px] justify-end items-center">
+                    <p>{dict.tech.info}</p>
+                    <h3>/////////////////////////////////////</h3>
+                </div>
+            </div>
+        </div>
+    );
 }
